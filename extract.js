8864eadcb574531c1e14027bfda64e952c54ee16
Andres Galindo <andres.galindo@flexshopper.com>
2015-03-04 12:29:52 -0500

    Extracting open sales now as well and making XLSX with tabs for each

--- extract.js
+++ extract.js
@@ -1,22 +1,20 @@
 var inspect = require('eyes').inspector({maxLength:20000});
 var pdfExtract = require('pdf-extract');
 var fs = require('fs');
-var csv = require('fast-csv');
+var excelbuilder = require('msexcel-builder');
 var parser = require('./parser.js');
 
 var argv = require('yargs')
     .usage('Usage: $0 <command> [options]')
-    .command('extract', 'Extract the contents of a PDF into a usable CSV and optionally route unmatchable payments to another file')
+    .command('extract', 'Extract the contents of a PDF into a usable XLSX')
     .demand(1)
-    .example('$0 extract -f file.pdf -o file.csv [-n no-matches.txt]', 'Convert the PDF to CSV')
+    .example('$0 extract -f file.pdf -o file.xlsx', 'Convert the PDF to CSV')
     .demand('f')
     .demand('o')
     .alias('f', 'file')
     .alias('o', 'out-file')
-    .alias('n', 'no-match-file')
     .nargs('f', 1)
     .nargs('o', 1)
-    .nargs('n', 1)
     .describe('f', 'PDF to load')
     .describe('o', 'Outfile to write to')
     .help('h')
@@ -26,12 +24,6 @@ var argv = require('yargs')
 
 var pdfPath = argv.f;
 var csvPath = argv.o;
-var noMatchFile = argv.n;
-
-var csvStream = csv.createWriteStream({headers: true});
-var writableStream = fs.createWriteStream(csvPath);
-
-csvStream.pipe(writableStream);
 
 var options = {type: 'text'};
 var processor = pdfExtract(pdfPath, options, function(err) {
@@ -42,6 +34,58 @@ var processor = pdfExtract(pdfPath, options, function(err) {
 
 
 processor.on('complete', function(data) {
-    p = parser.newParser(data);
-    console.log(p);
+    p = parser.newParser(data.text_pages);
+    data = p.parse();
+    workbook = excelbuilder.createWorkbook('./', csvPath);
+    if (data.openSales.length > 0) {
+        var openSalesSheet = workbook.createSheet('Open Sales', 8, data.openSales.length + 1);
+        openSalesSheet.set(1, 1, 'Type');
+        openSalesSheet.set(2, 1, 'Store Number');
+        openSalesSheet.set(3, 1, 'Trace Number');
+        openSalesSheet.set(4, 1, 'Merchant Reference Number');
+        openSalesSheet.set(5, 1, 'Check Number');
+        openSalesSheet.set(6, 1, 'Check Date');
+        openSalesSheet.set(7, 1, 'Credit Amount');
+        openSalesSheet.set(8, 1, 'Debit Amount');
+        for (i = 0; i < data.openSales.length; i++) {
+            var oLine = data.openSales[i];
+            var j = 0;
+            for (k in oLine) {
+                openSalesSheet.set(j+1, i+2, oLine[k]);
+                j++;
+            }
+        }
+    }
+
+    if (data.noMatches.length > 0) {
+        var noMatchesSheet = workbook.createSheet('No Matches', 1, data.noMatches.length);
+        for (i = 0; i < data.noMatches.length; i++) {
+            noMatchesSheet.set(1, i+1, data.noMatches[i]);
+        }
+    }
+
+    if (data.reversals.length > 0) {
+        var reversalsSheet = workbook.createSheet('Transactions', 4, data.reversals.length + 1);
+        reversalsSheet.set(1, 1, 'First Name');
+        reversalsSheet.set(2, 1, 'Last Name');
+        reversalsSheet.set(3, 1, 'Error Message');
+        reversalsSheet.set(4, 1, 'Reference Number');
+        for (i = 0; i < data.reversals.length; i++) {
+            var rLine = data.reversals[i];
+            var r = 0;
+            for (e in rLine) {
+                reversalsSheet.set(r+1, i+2, rLine[e]);
+                r++;
+            }
+        }
+    }
+
+    workbook.save(function(ok) {
+        if (ok || ok === null) {
+            console.log('File saved successfully');
+        } else {
+            workbook.cancel();
+            console.log('Failure saving file');
+        }
+    });
 });
--- parser.js
+++ parser.js
@@ -1,98 +1,175 @@
-var parser = function(data) {
-    this._data = data;
-};
+(function(exports) {
+    var parser = function(data) {
+        this._data = data;
+    };
 
-module.exports.newParser = parser;
 
-parser.prototype.parse = function() {
-    // Let's gather the day into lines we can worth with
-    var collectedLines = [];
-    var dataPackage = [];
-    dataToScan = this._data;
-    for (i = 0; i < dataToScan.length; i++) {
-        lines = dataToScan[i].split('\n');
-        for (a = 0; a < lines.length; a++) {
-            if (lines[a].indexOf('OPEN SALE') > -1
-                || lines[a].indexOf('VOID') > -1
-                || lines[a].indexOf('********') > -1) {
-                continue;
-            }
+    function isNumber(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    }
 
-            if ((
-                (
-                lines[a].indexOf('\/2015') >= 0
-                && lines[a].indexOf('$') >= 0
-                )
-                || lines[a].indexOf('CW: ') > -1
-                )
-                && lines[a].indexOf('to 02') == -1 ){
-                collectedLines.push(lines[a]);
-            }
+    /**
+     * Given a line from the CSV parse the OPEN SALE|VOID
+     * into a structure with meaning.
+     * @param openSaleLine
+     * @returns {{type: String, storeNumber: String, traceNumber: String, merchantRefNumber: String, checkNumber: String, checkDate: String, creditAmount: String}}
+     */
+    var parseOpenSale = function(openSaleLine) {
+        openSaleLine = openSaleLine.split(' ').filter(function(data) {
+            return data != ''
+        });
+
+        // OPEN SALE gets split into two, correct that...
+        type = openSaleLine[0];
+        data = openSaleLine.slice(1);
+        if (type == 'OPEN') {
+            type += ' ' + openSaleLine[1];
+            data = openSaleLine.slice(2);
         }
-    }
 
-    // Group into customer chunks...
-    collectedLines = collectedLines.splice(2);
-    chunks = [];
-    for (i = 0; i < collectedLines.length; i++) {
-        data = [];
-        data.push(collectedLines[i]);
-        i++;
-        data.push(collectedLines[i]);
-        chunks.push(data);
-    }
 
-    // Go over chunks and extract customer data
-    for (i = 0; i < chunks.length; i++) {
-        chunk = chunks[i];
+        // Manual payments are also split into two, correct that..
+        merchantRefNumber = data[2];
+        if (data[2].replace(/[A-Z]/g, '') == data[2]) {
+            merchantRefNumber = data[2] + ' ' + data[3];
+            tmpContainer = data.slice(0, 2);
+            tmpContainer = tmpContainer.concat([merchantRefNumber]);
+            tmpContainer = tmpContainer.concat(data.slice(4));
+            data = tmpContainer;
+        }
 
-        // Get the referenceId for the transaction
-        refIdMatches = chunk[0].match(/\s5\w{11}/);
-        refId = null;
-        if (refIdMatches == null) {
-            if (noMatchFile != undefined) {
-                fs.appendFile(noMatchFile, chunk.join(' ') + "\n\n");
-            }
-            continue;
+        // Handle debit and credits correctly..
+        debitAmount = 0;
+        creditAmount = 0;
+        if (data[6] != undefined) {
+            debitAmount = data[5];
+            creditAmount  = data[6];
         } else {
-            refId = refIdMatches[0].toString().trim();
+            creditAmount = data[5];
         }
 
-        // Get the name and error message for the customer
-        chunkParts = chunk[1].split(' ');
-        lastName = '';
-        fName    = [];
-        assignToName = true;
-        errorMessage = '';
-        for (j = 0; j < chunkParts.length; j++) {
-            if (chunkParts[j] != '') {
-                if (chunkParts[j].indexOf('2015') > -1) {
+
+        return {
+            type              : type,
+            storeNumber       : data[0],
+            traceNumber       : data[1],
+            merchantRefNumber : merchantRefNumber,
+            checkNumber       : data[3],
+            checkDate         : data[4],
+            creditAmount      : creditAmount,
+            debitAmount       : debitAmount
+        };
+    };
+
+    /**
+     *
+     * @returns {{reversals: Array, noMatches: Array, openSales: Array}}
+     */
+    parser.prototype.parse = function() {
+        // Let's gather the day into lines we can worth with
+        var collectedLines = [];
+        var dataPackage = {
+            reversals: [],
+            noMatches: [],
+            openSales: []
+        };
+
+        dataToScan = this._data;
+        for (i = 0; i < dataToScan.length; i++) {
+            lines = dataToScan[i].split('\n');
+            for (a = 0; a < lines.length; a++) {
+                if (lines[a].indexOf('OPEN SALE') > -1
+                    || lines[a].indexOf('VOID') > -1) {
+                    openSale = parseOpenSale(lines[a]);
+                    if (openSale != undefined) {
+                        dataPackage.openSales.push(openSale);
+                    }
                     continue;
                 }
 
-                if (chunkParts[j] == 'CW:') {
-                    j++;
-                    lastName = chunkParts[j];
-                    assignToName = true;
+                if (lines[a].indexOf('********') > -1) {
                     continue;
                 }
 
-                if (assignToName) {
-                    fName.push(chunkParts[j]);
-                } else {
-                    errorMessage += chunkParts[j] + ' ';
+                if ((
+                    (
+                    lines[a].indexOf('\/2015') >= 0
+                    && lines[a].indexOf('$') >= 0
+                    )
+                    || lines[a].indexOf('CW: ') > -1
+                    )
+                    && lines[a].indexOf('to 02') == -1 ){
+                    collectedLines.push(lines[a]);
                 }
-            } else {
-                assignToName = false;
             }
         }
 
+        // Group into customer chunks...
+        collectedLines = collectedLines.splice(2);
+        chunks = [];
+        for (i = 0; i < collectedLines.length; i++) {
+            data = [];
+            data.push(collectedLines[i]);
+            i++;
+            data.push(collectedLines[i]);
+            chunks.push(data);
+        }
 
-        fName = fName.join(' ');
-        errorMessage = errorMessage.trim();
-        dataPackage.push({first_name: fName, last_name: lastName, error_message: errorMessage, reference_id: refId});
-    }
+        // Go over chunks and extract customer data
+        for (i = 0; i < chunks.length; i++) {
+            chunk = chunks[i];
 
-    return dataPackage;
-};
+            // Get the referenceId for the transaction
+            refIdMatches = chunk[0].match(/\s5\w{11}/);
+            refId = null;
+            if (refIdMatches == null) {
+                dataPackage.noMatches.push(chunk.join(' '));
+                continue;
+            } else {
+                refId = refIdMatches[0].toString().trim();
+            }
+
+            // Get the name and error message for the customer
+            chunkParts = chunk[1].split(' ');
+            lastName = '';
+            fName    = [];
+            assignToName = true;
+            errorMessage = '';
+            for (j = 0; j < chunkParts.length; j++) {
+                if (chunkParts[j] != '') {
+                    if (chunkParts[j].indexOf('2015') > -1) {
+                        continue;
+                    }
+
+                    if (chunkParts[j] == 'CW:') {
+                        j++;
+                        lastName = chunkParts[j];
+                        assignToName = true;
+                        continue;
+                    }
+
+                    if (assignToName) {
+                        fName.push(chunkParts[j]);
+                    } else {
+                        errorMessage += chunkParts[j] + ' ';
+                    }
+                } else {
+                    assignToName = false;
+                }
+            }
+
+
+            fName = fName.join(' ');
+            errorMessage = errorMessage.trim();
+            dataPackage.reversals.push({first_name: fName, last_name: lastName, error_message: errorMessage, reference_id: refId});
+        }
+
+        return dataPackage;
+    };
+
+
+    exports.newParser = function(data) {
+        return new parser(data);
+    };
+})(module.exports);
 
